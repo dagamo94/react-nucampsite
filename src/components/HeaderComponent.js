f01621e8ed822708eafeb7eddef0f2f4f580f68c
dan <da.ga.mo.94@gmail.com>
2021-09-02 19:41:54 -0600

    Add header and footer components

---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand, Jumbotron } from 'reactstrap';
+
+
+class Header extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <Jumbotron fluid>
+                    <div className="container">
+                        <div className="row">
+                            <h1>NuCamp</h1>
+                            <h2> a better way to camp</h2>
+                        </div>
+                    </div>
+                </Jumbotron>
+                <Navbar dark sticky="top">
+                    <div className="container">
+                        <NavbarBrand href="/">Nucamp</NavbarBrand>
+                    </div>
+                </Navbar>
+            </React.Fragment>
+        );
+    }
+}
+
+export default Header;
